8e822ce7bcb1a41a08e5808b0155116c0afa89cc
Sergej <73028678+SergejK77@users.noreply.github.com>
2024-12-04 19:52:22 +0100

    Create FileSorter.py
    Sortiert die Dateien in den Zielordner nach der Dateiendung in den jeweiligen Ordner, welcher erzeugt wird und nach der Dateiendung benannt wird.

---
+++ FileSorter.py
@@ -0,0 +1,37 @@
+import os
+
+file_list = []
+extentions_list = list()
+sorted_dict = {}
+
+
+def sort_files(path):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            file_list.append(file)
+            ext = file.split(".")[1]
+            ext = "." + ext
+            if ext not in extentions_list:
+                extentions_list.append(ext)
+                sorted_dict[ext] = []
+            else:
+                pass
+            sorted_dict[ext].append([file])
+
+    print(sorted_dict)
+    for key in sorted_dict.keys():
+        ##print(f"Key: {key}")
+        pkey = path+key
+        if os.path.isfile(pkey) or os.path.isdir(pkey):
+            continue
+        if not os.path.isdir(pkey):
+            os.mkdir(pkey)
+        for a in sorted_dict.get(key):
+            for b in a:
+                if os.path.isfile(path+str(b)):
+                    os.rename(path + str(b), path + str(key) + '/' + str(b))
+
+
+if __name__ == "__main__":
+    fdir = "C:/Users/XX/YY/"
+    sort_files(fdir)
